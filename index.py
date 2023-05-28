45111201e58bbc1f9fe2e29e2c511744bb12fd84
rvariankaval <98034438+rvariankaval@users.noreply.github.com>
2023-05-28 16:20:11 -0400

    Update index.py

--- index.py
+++ index.py
@@ -10,7 +10,6 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'myaqimail@example.com'
 app.config['MAIL_PASSWORD'] = 'Masterman!2'
-
 mail = Mail(app)
 
 @app.route("/")
